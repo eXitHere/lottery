a3b5fdad7276fc84750df5c3ba7dd4e48b45bfff
eXit-Guy <39361943+eXitHere@users.noreply.github.com>
2021-04-01 23:52:26 +0700

    🚀

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router className='App'>
+			<Switch>
+				<Route
+					path='/:name/:number?'
+					render={(props) => <Main {...props} />}
+				></Route>
+			</Switch>
+		</Router>
+	);
 }
+
+const Main = () => {
+	let { name, number } = useParams();
+
+	return (
+		<div>
+			<div className='name'> {name}</div>
+			<div className='number_container'>
+				{number.split('').map((e) => {
+					return <BoxNumber num={e}></BoxNumber>;
+				})}
+			</div>
+			<div className='footer'>powered by eXit-Guy</div>
+		</div>
+	);
+};
+
+const BoxNumber = ({ num }) => {
+	const [open, setOpen] = useState(false);
+	const clickHandle = () => {
+		setOpen(true);
+	};
+	return (
+		<div onClick={clickHandle} className='number'>
+			{open && num}
+		</div>
+	);
+};
 
 export default App;
